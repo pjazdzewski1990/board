6b7492c80e0975cc371e34c10ca307dbdc70f57b
Patryk <pjazdzewski1990@gmail.com>
2018-10-25 22:34:44 +0200

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        Ths is a Card!
+      </div>
+    );
+  }
+}
+
+export default Card;
